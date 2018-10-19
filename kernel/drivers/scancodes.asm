dd709d8cdeb258e6c91bc624136160340fe00403
Fred Strauss <fred@strider.co.za>
2018-10-19 21:54:26 +0200

    Tidy up

--- kernel/drivers/scancodes.asm
+++ kernel/drivers/scancodes.asm
@@ -1,58 +1,17 @@
 scancodes:
 	db '?'		; unused
 	db '?'		; esc
-	db '1'
-	db '2'
-	db '3'
-	db '4'
-	db '5'
-	db '6'
-	db '7'
-	db '8'
-	db '9'
-	db '0'
-	db '-'
-	db '='
+	db '1234567890-='
 	db '?'		; backspace
 	db '?'		; tab
-	db 'q'
-	db 'w'
-	db 'e'
-	db 'r'
-	db 't'
-	db 'y'
-	db 'u'
-	db 'i'
-	db 'o'
-	db 'p'
-	db '['
-	db ']'
+	db 'qwertyuiop[]'
 	db '?'		; enter
 	db '?'		; left control
-	db 'a'
-	db 's'
-	db 'd'
-	db 'f'
-	db 'g'
-	db 'h'
-	db 'j'
-	db 'k'
-	db 'l'
-	db ';'
+	db 'asdfghjkl;'
 	db "'"
 	db '`'
 	db '?'		; left shift
-	db '\'
-	db 'z'
-	db 'x'
-	db 'c'
-	db 'v'
-	db 'b'
-	db 'n'
-	db 'm'
-	db ','
-	db '.'
-	db '/'
+	db '\zxcvbnm,./'
 	db '?'		; right shift
 	db '*'
 	db '?'		; left alt
