797afa1532eb991aa711068122c8a66acc595d49
Fred Strauss <fred@strider.co.za>
2018-11-02 17:52:20 +0200

    Work on keybuffer

--- kernel/drivers/keyboard.asm
+++ kernel/drivers/keyboard.asm
@@ -4,10 +4,14 @@ KEYB_C	equ 0x64	; Keyboard command port
 KEYB_D	equ 0x60	; Keyboard data port
 
 ; key buffer
-BUFFERSIZE 	equ 256				; Max size of keybuffer
+BUFFERSIZE 	equ 5				; Max size of keybuffer
 keybuffer 	resb BUFFERSIZE		; Key buffer
-keybufferi 	db 0				; Current key buffer index
+.start	 	db 0				; Current key buffer start index
+.end		db 0				; Current key buffer end index
 scancode	db 0				; Store current scancode
+
+; debug current keybuffer as null terminated string
+dbgkeybuffer	resb BUFFERSIZE+1	; +1 for null terminated
 
 MSG_ENTER db 0x0d, 0x0a, "> ", 0
 MSG_BACKSPACE db 0x0e, 0
@@ -32,12 +36,37 @@ keyboard_int:
 	je .backspace
 
 	; key pressed, add it to keybuffer
-	;mov bx, keybufferi
-	;mov ax, [scancode]
-	;mov [keybuffer+bx], al
-	;inc bx						; Todo check against BUFFERSIZE
-	;mov [keybufferi], bl
+	mov bx, [keybuffer.end]
+	mov ax, [scancode]
+	mov [keybuffer+bx], al
+	inc bx						
+	cmp bx, BUFFERSIZE-1	; check against BUFFERSIZE
+	jbe .continue
 
+	; key buffer should wrap round
+	; ring buffer
+	mov bl, 0				; end to 0
+	mov byte [keybuffer.start], 1
+
+.continue:
+	mov [keybuffer.end], bl
+
+	; if end has wrapped round to start, inc start
+	cmp [keybuffer.start], bl
+	jne .print
+
+	mov bx, [keybuffer.start]
+	inc bx
+
+	cmp bx, BUFFERSIZE-1	; check it against BUFFERSIZE
+	jbe .continue2
+
+	mov byte [keybuffer.start], 0
+
+.continue2:
+	mov [keybuffer.start], bl
+
+.print:
 	; and print it to the screen
 	mov bx, 0
 	mov bl, [scancode]
@@ -55,9 +84,24 @@ keyboard_int:
 	;mov byte [keybufferi], 0		; clear keybuffer
 	jmp .done
 
-.backspace
+.backspace:
 	mov bx, MSG_BACKSPACE
 	call kprint
+
+	; decrement end, unless it's already at start
+	mov al, [keybuffer.end]
+	cmp [keybuffer.start], al
+	jne .done
+
+	cmp al, 0
+	je .endatend
+
+	dec ax
+	mov [keybuffer.end], al
+	jmp .done
+
+.endatend:
+	mov byte [keybuffer.end], BUFFERSIZE-1
 	;jmp .done
 
 .done:
@@ -65,4 +109,20 @@ keyboard_int:
 	pop bx
 	pop ax
 	ret
+
+; get current string from keybuffer as null terminated string
+; ax = size of the user provided string space
+; bx = address of area to return string
+gets:
+	push ax
+	push bx
+	push cx
+
+	; see which is less, the size of our data or the size
+	; requested, use the smallest one
+	
+	ret
+	pop cx
+	pop bx
+	pop ax
 
