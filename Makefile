9796f16053e77256fb12a4c33732f8c2fe6d8894
Fred Strauss <fred@strider.co.za>
2018-11-05 13:55:41 +0200

    Very bare kernel booting with grub

--- Makefile
+++ Makefile
@@ -1,28 +1,43 @@
 GDB=gdb
+LODEVICE := $(shell sudo losetup -f)
 
 .PHONY: all
 
-all: floppy.bin
+#all: floppy.bin
+all: disk.img
 
 clean:
 	rm -f floppy.bin
 	rm -f bootsector/*.o bootsector/*.bin
-	rm -f kernel/*.o kernel/*.bin
+	rm -f kernel/*.o kernel/*.bin kernel/*.elf
+	rm -f disk.img
 
-run: floppy.bin
-	qemu-system-i386 -drive format=raw,index=0,if=floppy,file=$<
+#run: floppy.bin
+#	qemu-system-i386 -drive format=raw,index=0,if=floppy,file=$<
+
+run: disk.img
+	qemu-system-i386 -drive format=raw,index=0,file=$<
+
+disk.img: grubdisk.img grub.cfg finux.bin
+	cp -f $< $@
+	mkdir mnt
+	sudo losetup $(LODEVICE) $@ -o 1048576
+	sudo mount $(LODEVICE) mnt
+	sudo cp grub.cfg mnt/boot/grub/
+	sudo cp finux.bin mnt/boot/
+	sudo umount mnt
+	rmdir mnt
+	sudo losetup -d $(LODEVICE)
+
+finux.bin: kernel/kernel.elf linker.ld
+	#ld -n -o $@ -T linker.ld -m elf_i386 -Ttext 0x1000 kernel/kernel.elf
+	ld -n -o $@ -T linker.ld -m elf_i386 kernel/kernel.elf
+
+kernel/kernel.elf: kernel/*.asm kernel/*/*.asm lib/*.asm
+	nasm -i kernel/ -f elf -o $@ kernel/kernel.asm
 
 # see .gdbinit for gdb settings
-debug: floppy.bin
+debug: disk.img
 	qemu-system-i386 -S -s \
-			-drive format=raw,index=0,if=floppy,file=$< &	
+			-drive format=raw,index=0,file=$< &	
 	$(GDB)
-
-floppy.bin: bootsector/bootsector.bin kernel/kernel.bin
-	cat $^ > $@
-
-bootsector/bootsector.bin: bootsector/*.asm
-	nasm -i bootsector/ -f bin -o $@ bootsector/bootsector.asm
-
-kernel/kernel.bin: kernel/*.asm kernel/*/*.asm lib/*.asm
-	nasm -i kernel/ -f bin -o $@ kernel/kernel.asm
--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -1,38 +1,64 @@
-[org 0x1000]	; Kernel will be loaded to this address
+;[org 0x1000]	; Kernel will be loaded to this address
+global start
+
+MAGIC 			equ 0xE85250D6
+ARCHITECTURE 	equ 0
+HEADERSIZE 		equ header_end - header_start
+CHECKSUM 		equ 0x100000000 - (MAGIC+HEADERSIZE+ARCHITECTURE)
+
+section .multiboot_header
+header_start:
+	align 4
+	dd MAGIC			; multiboot 2 magic number
+	dd ARCHITECTURE		; architecture (32-bit protected mode)
+	dd HEADERSIZE		; header length
+	dd CHECKSUM
+	; tags come here
+	; end tag
+	dw 0				; type
+	dw 0				; flags
+	dw 8				; size
+header_end:
+
+section .text
 [bits 32]		; We're in protected mode
+start:
+mov byte [0xb8000], 'X'
+mov ax, 0xBADF
+hlt
 
-mov bx, MSG_NEWLINE
-call kprint
-mov bx, MSG_KERNEL
-call kprint
-mov bx, MSG_NEWLINE
-call kprint
+;mov bx, MSG_NEWLINE
+;call kprint
+;mov bx, MSG_KERNEL
+;call kprint
+;mov bx, MSG_NEWLINE
+;call kprint
 
-; load idt
-mov bx, MSG_IDT
-call kprint
-lidt [idt_reg]
-mov bx, MSG_NEWLINE
-call kprint
+;; load idt
+;mov bx, MSG_IDT
+;call kprint
+;lidt [idt_reg]
+;mov bx, MSG_NEWLINE
+;call kprint
 
-; initialise PICs
-mov bx, MSG_PIC
-call kprint
-call init_pic
-mov bx, MSG_NEWLINE
-call kprint
+;; initialise PICs
+;mov bx, MSG_PIC
+;call kprint
+;call init_pic
+;mov bx, MSG_NEWLINE
+;call kprint
 
-sti				; Enable interrupts
+;sti				; Enable interrupts
 
-call shell_main
+;call shell_main
 
-jmp $			; Infinite loop
+;jmp $			; Infinite loop
 
 %include "kprint.asm"
-%include "interrupts.asm"
-%include "idt.asm"
-%include "pic.asm"
-%include "shell.asm"
+;%include "interrupts.asm"
+;%include "idt.asm"
+;%include "pic.asm"
+;%include "shell.asm"
 
 ; data
 MSG_NEWLINE	db 0x0d, 0x0a, 0
